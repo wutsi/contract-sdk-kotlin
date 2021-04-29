52844de6810d4158e9e9a150878f7f0aa15675e9
htchepannou <htchepannou@expedia.com>
2021-04-29 07:02:19 -0400

    filter active contract by date

--- src/main/kotlin/com/wutsi/contract/ContractApi.kt
+++ src/main/kotlin/com/wutsi/contract/ContractApi.kt
@@ -1,9 +1,11 @@
 package com.wutsi.contract
 
 import com.wutsi.contract.dto.SearchContractResponse
+import feign.Param
 import feign.RequestLine
+import java.time.LocalDate
 
 public interface ContractApi {
-  @RequestLine("GET /v1/contracts/active")
-  public fun active(): SearchContractResponse
+  @RequestLine("GET /v1/contracts/active?date={date}")
+  public fun active(@Param("date") date: LocalDate? = null): SearchContractResponse
 }
